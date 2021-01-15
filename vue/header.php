f33fc2c1c2162f6250d9e772cd811e8bf7ed0db2
peichedev <peichedev@github.com>
2021-01-15 15:46:20 +0100

    message de conflit

--- vue/header.php
+++ vue/header.php
@@ -3,17 +3,62 @@
 <head>
 	<title>ForumPAD</title>
 	<meta charset='utf-8'>
-    <link href="css/FStyle.css" rel="stylesheet">
+    <link href="vue/FStyle.css" rel="stylesheet">
 
 </head>
 <body>
 <header>
-	<nav>
-        <ul>
-            <li><a href="default.html">Home</a></li>
-            <li><a href="news.asp">News</a></li>
-            <li><a href="contact.asp">Contact</a></li>
-            <li><a href="about.asp">About</a></li>
-        </ul>
-    </nav>
+    <div id='totalmenu'>
+        <nav id='navmenuuser'>
+            <div>
+                    
+            </div>
+
+            <ul id='menuuser'>
+                <li id='bouttonmenu'><a href=#>Déconnexion</a></li>
+                <li id='bouttonmenu'><a href=#>log in</a></li>
+                <li><a href=#> <img src="img/avatar.png" class='avatar' alt=""></a></li>
+            </ul>
+        </nav>
+    
+        <nav id='navmenu'>
+            <a href="index.php"><img id='imgforum' src='img/forum.jpg'></a>
+            <ul id='navmenu'>
+                <li>
+                    <style>
+                        #search {
+                            border-radius: 30px;
+                            border: 2px solid black;
+                        }
+
+                        #button {
+                            background-color: aqua;
+                            border: 0px solid black;
+                            height: 30px;
+                            border-radius: 30px;
+                            cursor: pointer;
+                        }
+                    </style>
+                    <input type="search" id="search" value="" onchange="ouvrirPage()">
+                    <input type="button" id="button" onclick="ouvrirPage()" value="Chercher">
+                    <script>
+                        function ouvrirPage() {
+                            var a = document.getElementById("search").value;
+
+                            if (a === "le mot a marquer") {
+                                window.open("l'id de la recherche");
+                            }
+
+                            if (a === "le mot a marquer") {
+                                window.open("l'id de la recherche");
+                            }
+
+                            if (a === "le mot a marquer") {
+                                window.open("l'id de la recherche");
+                            }
+                        }
+                    </script></li>
+            </ul>
+        </nav>
+    </div>
 </header>
