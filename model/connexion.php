5823c0d648b61b60d98038ea15c14dd76e3685be
alancot31 <alancottard@gmail.com>
2021-01-22 15:44:23 +0100

    first commit

--- model/connexion.php
+++ model/connexion.php
@@ -1,8 +1,8 @@
 <?php
+	$dsn ='mysql:host=localhost;dbname=forum';
 	$db_username = 'root';
     $db_password = '';
     $db_name     = 'forum';
-    $db_host     = 'localhost';
-    $db = mysqli_connect($db_host, $db_username, $db_password,$db_name)
+    $db = new PDO($dsn, $db_username, $db_password)
   	or die('could not connect to database');
 ?>
--- model/verification.php
+++ model/verification.php
@@ -3,14 +3,14 @@
   if(isset($_POST['username']) && isset($_POST['password']))
   {
     require('connexion.php');
-    $username = mysqli_real_escape_string($db,htmlspecialchars($_POST['username'])); 
+    $username = htmlspecialchars($_POST['username']); 
     $password = sha1(htmlspecialchars($_POST['password']));  
     if($username !== "" && $password !== "")
     {
       $requete = $db->prepare ('SELECT count(*) FROM users where pseudo = ? and mdp = ? ');
-      $requete->execute(array($username,$password));
-      $count = $reponse['count(*)'];
-      if($count!=0)
+      $requete->execute(array($username ,$password));
+      $results = $requete->fetchAll();
+      if($results!=0)
       {
        $_SESSION['username'] = $username;
        header('Location: ../index.php');
