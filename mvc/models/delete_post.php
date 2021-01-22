0df2c523b5886a269c50e631aac568247cf7039c
peichedev <peichedev@github.com>
2021-01-22 13:43:35 +0100

    suppression dossier

--- mvc/models/delete_post.php
+++
@@ -1,5 +0,0 @@
-<?php
-function supprimer($bdd){  
-    $reponse = $bdd->prepare('delete from articles where id=?');
-    $reponse->execute(array($_GET['id']));}
-?>
--- mvc/view/header.php
+++
@@ -1,8 +0,0 @@
-<!DOCTYPE html>
-<html lang="fr">
-	<head>
-		<title>TD_decouverte-MVC</title>
-		<meta charset="utf-8">
-	</head>
-	<body>
-        <p>Bienvenue sur ma page</p>
