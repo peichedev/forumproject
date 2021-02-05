03896e9ebb783c9bc21e076e20a3e3fd9efeb84c
alancot31 <alancottard@gmail.com>
2021-02-05 15:43:48 +0100

    alan

--- model/connexion.php
+++ model/connexion.php
@@ -2,8 +2,7 @@
 	$dsn ='mysql:host=localhost;dbname=forum';
 	$db_username = 'root';
   $db_password = '';
-  $db_name     = 'forum';
-  //$db = new PDO($dsn, $db_username, $db_password) or die('could not connect to database');
+  $db_name = 'forum';
 
   try {
     $bdd = new PDO($dsn,$db_username,$db_password);
--- model/verification.php
+++ model/verification.php
@@ -1,5 +1,4 @@
-<?php
-  
+<?php 
   session_start();
   if(isset($_POST['username']) && isset($_POST['password']))
   {
@@ -8,23 +7,23 @@
     $password = sha1(htmlspecialchars($_POST['password']));  
     if($username !== "" && $password !== "")
     {
-      $requete = $bdd->prepare ('SELECT count(*) FROM users where pseudo = ? and mdp = ? ');
-      $requete->execute(array($username ,$password));
-      $results = $requete->fetchAll();
-      if($results!=0)
-      {
-       $_SESSION['username'] = $username;
-       header('Location: ../index.php');
-      }
-      else
-      {
-        header('Location: login.php?erreur=1');
-      }
+      $requete = $bdd->prepare ('SELECT * FROM users where pseudo = ? and mdp = ? ');
+     	$requete->execute(array($username ,$password));
+    	$result=$requete->rowCount();
+      echo $result;
+    	if($result!=0)
+      	{
+       		$_SESSION['username'] = $username;
+          header('Location: ../index.php');
+      	}
+      	else
+      	{
+        	header('Location: ../vue/login.php?erreur=1');
+      	}
     }
     else
     {
-      header('Location: login.php?erreur=2');
+      header('Location: ../vue/login.php?erreur=2');
     }
   }
-  mysqli_close($db);
 ?>
